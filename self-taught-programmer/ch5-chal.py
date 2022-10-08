1de7b8aab6f059867042757390ef6c4dcf033afa
Paula Letícia Gerónimo <paulagero182@gmail.com>
2022-10-08 18:58:20 -0500

    Challenge 3

--- self-taught-programmer/ch5-chal.py
+++ self-taught-programmer/ch5-chal.py
@@ -13,6 +13,15 @@ and prints it
 def stringParam(string: str):
   print(string)
   
+'''
+write a function that takes three required parameters
+and two optional parameters
+'''
+def params(x:int, y:int, z:int, a=1, b=2):
+  print(x, y, z, a, b)
+
 if __name__ == '__main__':
   print(square(2))
   stringParam("Howdy world!")
+  params(10, 20, 30)
+  params(10, 20, 30, 40, 50)
