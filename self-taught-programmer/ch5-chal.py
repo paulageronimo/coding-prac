07afce0812a3d03214189d572e656c712bec4d1d
Paula Letícia Gerónimo <paulagero182@gmail.com>
2022-10-08 18:54:57 -0500

    Challenge 1

--- self-taught-programmer/ch5-chal.py
+++ self-taught-programmer/ch5-chal.py
@@ -1,1 +1,10 @@
 
+'''
+write a function that takes a number as an input and 
+returns that number squared
+'''
+def square(num: int):
+  return num**2
+
+if __name__ == '__main__':
+  print(square(2))
