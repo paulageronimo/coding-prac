dfa4c52ebde1dfdb8b3401ebc561f6debaee8a00
Paula Letícia Gerónimo <paulagero182@gmail.com>
2022-10-08 19:15:36 -0500

    Challenge 6

--- self-taught-programmer/ch5-chal.py
+++ self-taught-programmer/ch5-chal.py
@@ -1,33 +1,36 @@
 
-'''
-write a function that takes a number as an input and 
-returns that number squared
-'''
 def square(num: int):
+  """
+  write a function that takes a number as an input and 
+  returns that number squared
+  """
+
   return num**2
 
-'''
-createa a function that accepts a string as a parameter
-and prints it
-'''
+
 def stringParam(string: str):
+  """
+  createa a function that accepts a string as a parameter
+  and prints it
+  """
   print(string)
   
-'''
-write a function that takes three required parameters
-and two optional parameters
-'''
+
 def params(x:int, y:int, z:int, a=1, b=2):
+  """
+  write a function that takes three required parameters
+  and two optional parameters
+  """
   print(x, y, z, a, b)
 
-'''
-write a function that converts a string to a float and returns
-the result. 
-
-use exception handling to catch the exception that
-could occur.
-'''
 def str2float(string: str):
+  """
+  write a function that converts a string to a float and returns
+  the result. 
+
+  use exception handling to catch the exception that
+  could occur.
+  """
   try:
     flt = float(string)
   except ValueError:
--- self-taught-programmer/ch5-chal4.py
+++ self-taught-programmer/ch5-chal4.py
@@ -1,23 +1,27 @@
 '''
 write a program with two functions. 
-
-The first function should take an integer as a 
-parameter and return the result of the integer 
-divided by 2.
-
-The second function should take an integer as a parameter
-and return the result of the integer multiplied by 4.
-
-Call the first funciton, save the result as a variable, and 
-pass it as a parameter to the second function.
 '''
+
 def first(num: int):
+  """
+  The first function should take an integer as a 
+  parameter and return the result of the integer 
+  divided by 2.
+  """
   return num/2
 
 def second(num: int):
+  """
+  The second function should take an integer as a parameter
+  and return the result of the integer multiplied by 4.
+  """
   return num * 4
 
 if __name__ == '__main__':
+  """
+  Call the first funciton, save the result as a variable, and 
+  pass it as a parameter to the second function.
+  """
   var = first(5)
   print(var)
   
