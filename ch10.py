6ba71e11d9aa8082599588b07dca4a007e634b2f
Paula Letícia Gerónimo <paulagero182@gmail.com>
2022-10-08 19:56:40 -0500

    Challenge 1 (EXTRA CHALLENGING)
    Word is selected randomly from a list of words. Code is all my own :100:

--- ch10.py
+++ ch10.py
@@ -1,3 +1,5 @@
+import random
+
 def hangman(word):
   wrong = 0
   stages = ["",
@@ -37,4 +39,24 @@ def hangman(word):
     print("You lose! It was {}.".format(word))
 
 if __name__ == '__main__':
-  hangman("cat")
+  # random lists of words I decided to come up with.
+  words= {  "easy": ["cat", "run", "car", "tea", "red", "pot"],
+            "medium": ["mouse", "apple", "tree", "doubt"],
+            "hard": ["johnson", "mediocre", "monopoly", "juxtaposition", "xylophone"]}
+  
+  level = input("Input difficulty.\nOptions:\n - easy\n - medium \n - hard\n")
+  
+  # continues game even if input is not in list
+  # by generating random difficulty
+  if level not in words:
+    level = list(words)[random.randrange(3)]
+  
+  # gets length of the list, since they're all different sizes
+  # this reduces the need to hard code numbers.
+  lenSubList = len(words[level])
+  
+  # gets the words list from the respective level 
+  # then generates the random index of the hangman word.
+  word = words[level][random.randrange(lenSubList)]
+  
+  hangman(word)
