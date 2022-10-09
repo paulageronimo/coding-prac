2bb5e5ccba1c5d3062e0d03b1155f8b8dd9df358
Paula Letícia Gerónimo <paulagero182@gmail.com>
2022-10-08 20:01:49 -0500

    Layout changes from the book's example

--- ch10.py
+++ ch10.py
@@ -14,6 +14,7 @@ def hangman(word):
   rletters = list(word)
   board = ["__"] * len(word)
   win = False
+  
   print("Welcome to Hangman")
     
   while wrong < len(stages) - 1:
@@ -26,14 +27,17 @@ def hangman(word):
       rletters[cind] = '$'
     else:
       wrong += 1
+    
     print((" ".join(board)))
     e = wrong + 1
     print("\n".join(stages[0: e]))
+    
     if "__" not in board:
       print("You win!")
       print(" ".join(board))
       win = True
       break
+    
   if not win:
     print("\n".join(stages[0: wrong]))
     print("You lose! It was {}.".format(word))
